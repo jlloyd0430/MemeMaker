e477372102ab51eb94f48427edf5e13ae29e8569
Jesse Lloyd <116366380+jlloyd0430@users.noreply.github.com>
2023-03-01 09:07:41 -0500

    Update Header.js
    Add menu icon

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState, Fragment } from "react";
 import logo from "./images/pepe.png";
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [currentPage, setCurrentPage] = useState("");
+
+  const toggleMenu = (page) => {
+    setMenuOpen(!menuOpen);
+    setCurrentPage(page);
+  };
+
   return (
     <header>
-      <div className="logo-title">
-        <img src={logo} alt="meme" class="logo" />
-        <h1 className="title">Meme Dashboard</h1>
+      <Fragment>
+        {menuOpen ? (
+          <div className="menu">
+            <ul>
+              <li onClick={() => toggleMenu("derpbirds")}>Derpbirds</li>
+              <li onClick={() => toggleMenu("dbagmfers")}>Dbagmfers</li>
+            </ul>
+          </div>
+        ) : (
+          <div className="logo-title">
+            <img src={logo} alt="meme" className="logo" />
+            <h1 className="title">Meme Dashboard</h1>
+          </div>
+        )}
+      </Fragment>
+      <div className="menu-icon" onClick={() => toggleMenu("")}>
+        <i className="fa fa-bars"></i>
       </div>
     </header>
   );
 }
+
